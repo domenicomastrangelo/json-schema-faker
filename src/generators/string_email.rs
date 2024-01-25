b39b682fb9165da19364bc850cc56676f3a501c3
Domenico Mastrangelo <dome@domenicomastrangelo.com>
2024-01-25 21:45:18 +0100

    Refactor string_email generator to include name, surname, and random number in generated email

--- src/generators/string_email.rs
+++ src/generators/string_email.rs
@@ -1,3 +1,20 @@
+use super::{string_name, string_surname};
+use rand::Rng;
+
 pub fn generate() -> serde_json::Value {
-    serde_json::json!("hello world")
+    let name = match string_name::generate() {
+        serde_json::Value::String(name) => name,
+        _ => return serde_json::json!(null),
+    };
+    let surname = match string_surname::generate() {
+        serde_json::Value::String(surname) => surname,
+        _ => return serde_json::json!(null),
+    };
+
+    let mut rng = rand::thread_rng();
+    let random_number: u8 = rng.gen_range(50..99);
+
+    let email = format!("{}.{}{}@example.com", name, surname, random_number);
+
+    serde_json::json!(email.to_lowercase())
 }
