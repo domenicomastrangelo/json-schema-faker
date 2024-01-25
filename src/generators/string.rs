a6906f88af351dd80cc53061675f35bebd2ef100
Domenico Mastrangelo <dome@domenicomastrangelo.com>
2024-01-25 19:40:42 +0100

    Add string generators for address, birthday, city, country, email, phone, state, street, and zip

--- src/generators/string.rs
+++ src/generators/string.rs
@@ -1,35 +1,20 @@
-use super::string_name;
+use super::{
+    string_address, string_birthday, string_city, string_country, string_email, string_name,
+    string_phone, string_state, string_street, string_zip,
+};
 
 pub fn generate(t: &str) -> serde_json::Value {
     match t {
         "name" => string_name::generate(),
-        "email" => email::generate(),
-        "phone" => phone::generate(),
-        "birthday" => birthday::generate(),
-        "address" => address::generate(),
+        "email" => string_email::generate(),
+        "phone" => string_phone::generate(),
+        "birthday" => string_birthday::generate(),
+        "address" => string_address::generate(),
+        "city" => string_city::generate(),
+        "country" => string_country::generate(),
+        "state" => string_state::generate(),
+        "zip" => string_zip::generate(),
+        "street" => string_street::generate(),
         _ => serde_json::json!(null),
     }
-}
-
-mod email {
-    pub fn generate() -> serde_json::Value {
-        serde_json::json!("hello world")
-    }    
-}
-
-mod phone {
-    pub fn generate() -> serde_json::Value {
-        serde_json::json!("hello world")
-    }    
-}
-
-mod birthday {
-    pub fn generate() -> serde_json::Value {
-        serde_json::json!("hello world")
-    }    
-}
-mod address {
-    pub fn generate() -> serde_json::Value {
-        serde_json::json!("hello world")
-    }    
 }
